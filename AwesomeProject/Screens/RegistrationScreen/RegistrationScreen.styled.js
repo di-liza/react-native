d6e397fbfe36d8d11ea9aecb675d8d738a47b35b
Liza_Diachenko01~ <liza.diachenko.99@gmail.com>
2023-05-31 19:46:59 +0300

    add some basic styles

--- AwesomeProject/Screens/RegistrationScreen/RegistrationScreen.styled.js
+++ AwesomeProject/Screens/RegistrationScreen/RegistrationScreen.styled.js
@@ -3,12 +3,25 @@ import { StyleSheet } from "react-native";
 export const stylesReg = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
     boxSizing: "border - box",
+    fontFamily: "Roboto",
   },
   image: {
     flex: 1,
     justifyContent: "center",
+  },
+  scrollView: {
+    backgroundColor: "#FFFFFF",
+    marginHorizontal: 20,
+    height: 549,
+    width: 375,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    marginTop: 263,
+    paddingTop: 92,
+    paddingLeft: 16,
+    paddingRight: 16,
+    textAlign: "center",
   },
   text: {
     color: "#212121",
@@ -19,19 +32,45 @@ export const stylesReg = StyleSheet.create({
     lineHeight: 35,
   },
   button: {
-    color: "#fff",
+    width: 343,
+    paddingTop: 16,
+    paddingBottom: 16,
+    marginTop: 32,
+
     borderRadius: 100,
     marginBottom: 16,
+    backgroundColor: "#FF6C00",
+    borderRadius: 100,
+
+    textAlign: "center",
+    color: "#fff",
+    fontSize: 16,
+    lineHeight: 19,
   },
   input: {
     height: 50,
     width: 343,
+
+    paddingTop: 16,
+    paddingBottom: 15,
+    paddingLeft: 16,
+    marginBottom: 16,
+
     backgroundColor: "#F6F6F6",
+
     borderColor: "#E8E8E8",
     borderWidth: 1,
     borderStyle: "solid",
+    borderRadius: 8,
+
     color: "#BDBDBD",
     fontSize: 16,
     lineHeight: 19,
+  },
+  link: {
+    color: "#1B4371",
+    fontSize: 16,
+    lineHeight: 19,
+    textAlign: "center,",
   },
 });
